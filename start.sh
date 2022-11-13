0723b1d9309eaf7edcce403e71df071d681f1302
Avaer Kazmer <a@modules.io>
2022-11-12 19:31:01 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 conda activate ovseg
-sudo $(which python3) demo.py --config-file configs/ovseg_swinB_vitL_demo.yaml --class-names 'person' 'water' 'flower' 'mat' 'fog' 'land' 'grass' 'field' 'dirt' 'metal' 'light' 'book' 'leaves' 'mountain' 'tree' 'gravel' 'wood' 'bush' 'bag' 'food' 'path' 'stairs' 'rock' 'house' 'clothes' 'animal' --input ./dalle5.png --output ./pred5 --opts MODEL.WEIGHTS ./ovseg_swinbase_vitL14_ft_mpt.pth
+# sudo $(which python3) demo.py
+sudo nohup $(which python3) demo.py &
